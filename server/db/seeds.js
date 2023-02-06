f938f42dc11af9fdc4d3870259e0b82c86abf444
Costicus <yaremenko.konstantin@gmail.com>
2023-02-06 12:25:09 +0000

    Added seed file

---
+++ server/db/seeds.js
@@ -0,0 +1,64 @@
+use mat_tracker;
+db.dropDatabase();
+
+db.activities.insertMany([
+    {
+        exercise: 
+        {
+            name: "Yoga",
+            intensity: "moderate"
+        },
+        time: 30,
+        day: "Monday"
+    },
+
+    {
+        exercise: 
+        {
+            name: "Running",
+            intensity: "vigorous"
+        },
+        time: 30,
+        day: "Monday"
+    },
+
+    {
+        exercise: 
+        {
+            name: "Pilates",
+            intensity: "moderate"
+        },
+        time: 60,
+        day: "Tuesday"
+    },
+
+    {
+        exercise: 
+        {
+            name: "Swimming",
+            intensity: "vigorous"
+        },
+        time: 60,
+        day: "Tuesday"
+    },
+
+    {
+        exercise: 
+        {
+            name: "Martial Arts",
+            intensity: "vigorous"
+        },
+        time: 60,
+        day: "Wednesday"
+    },
+
+    {
+        exercise: 
+        {
+            name: "Walking",
+            intensity: "moderate"
+        },
+        time: 45,
+        day: "Friday"
+    },
+]);
